88841dbb804e0f6c56b3f0f7e41f6b4e1ed69183
tarashukla <ts6796@princeton.edu>
2023-02-13 19:46:26 -0500

    more pointers

--- decomment.c
+++ decomment.c
@@ -11,14 +11,14 @@ void handle_Norm(char c, enum State *current);
 void handle_fSlash(char c, enum State *current);
 void handle_sStar(char c, enum State *current);
 void handle_eStar(char c, enum State *current);
-void handle_bSlash(char c, enum State current);
-void handle_sq_bSlash(char c, enum State current);
-void handle_dq_bSlash(char c, enum State current);
-void handle_star_bSlash(char c, enum State current);
-void handle_sString(char c, enum State current);
-void handle_eString(char c, enum State current);
-void handle_sChar(char c, enum State current);
-void handle_eChar(char c, enum State current);
+void handle_bSlash(char c, enum State *current);
+void handle_sq_bSlash(char c, enum State *current);
+void handle_dq_bSlash(char c, enum State *current);
+void handle_star_bSlash(char c, enum State *current);
+void handle_sString(char c, enum State *current);
+void handle_eString(char c, enum State *current);
+void handle_sChar(char c, enum State *current);
+void handle_eChar(char c, enum State *current);
 
 
 int checkState(char c, enum State *current){
@@ -27,14 +27,14 @@ int checkState(char c, enum State *current){
     if (*current == fSlash) handle_fSlash(c, current);
     if (*current == sStar) handle_sStar(c, current);
     if (*current ==eStar) handle_eStar(c,current);
-    if (current ==sString) handle_sString(c,current);
-    if (current == eString) handle_eString(c,current);
-    if (current == eChar) handle_eChar(c,current);
-    if (current == sChar) handle_sChar(c,current);
-    if (current == bSlash) handle_bSlash(c,current);
-    if (current == sq_bSlash) handle_sq_bSlash(c,current);
-    if (current == dq_bSlash) handle_dq_bSlash(c,current);
-    if (current == star_bSlash) handle_star_bSlash(c,current);
+    if (*current ==sString) handle_sString(c,current);
+    if (*current == eString) handle_eString(c,current);
+    if (*current == eChar) handle_eChar(c,current);
+    if (*current == sChar) handle_sChar(c,current);
+    if (*current == bSlash) handle_bSlash(c,current);
+    if (*current == sq_bSlash) handle_sq_bSlash(c,current);
+    if (*current == dq_bSlash) handle_dq_bSlash(c,current);
+    if (*current == star_bSlash) handle_star_bSlash(c,current);
 
 }
  void handle_Norm(char c, enum State *current){
@@ -86,7 +86,7 @@ void handle_eStar(char c, enum State *current){
     else current = sStar;
 }
 
-void handle_bSlash(char c, enum State current){
+void handle_bSlash(char c, enum State *current){
     if (c=='n') putchar('/n');
     else {
         putchar('\\');
@@ -95,37 +95,37 @@ void handle_bSlash(char c, enum State current){
     }
 }
 
-void handle_sq_bSlash(char c, enum State current){
+void handle_sq_bSlash(char c, enum State *current){
     if (c=='n') putchar('/n');  
     current = sChar;
 }
-void handle_dq_bSlash(char c, enum State current){
+void handle_dq_bSlash(char c, enum State *current){
     if (c=='n') putchar('/n');
     current = sString;
 }
-void handle_star_bSlash(char c, enum State current){
+void handle_star_bSlash(char c, enum State *current){
     if (c=='n') putchar('/n');
     current = sStar;
 }
 
-void handle_sString(char c, enum State current){
+void handle_sString(char c, enum State *current){
     if (c=='"') current = eString;
     if (c=='\\') current = dq_bSlash;
     else current = sString;
 }
 
-void handle_eString(char c, enum State current){
+void handle_eString(char c, enum State *current){
     if (c=='/') current = fSlash;
     else current = normal;
 }
 
-void handle_sChar(char c, enum State current){
+void handle_sChar(char c, enum State *current){
     if (c=='\'') current =eChar;
     if (c=='\\') current = sq_bSlash;
     else current = sChar;
 }
 
-void handle_eChar(char c, enum State current){
+void handle_eChar(char c, enum State *current){
     current = normal; /*handle normal?*/
 }
 
